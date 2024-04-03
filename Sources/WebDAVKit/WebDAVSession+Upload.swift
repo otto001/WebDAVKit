917f56c71f0e7dac578f9a84cb8eb7b10ce14c29
Matteo Ludwig <mludwig@posteo.de>
2024-04-03 15:47:01 +0200

    Added method for creating upload tasks suitable for background uploads

--- Sources/WebDAVKit/WebDAVSession+Upload.swift
+++ Sources/WebDAVKit/WebDAVSession+Upload.swift
@@ -21,7 +21,7 @@ extension WebDAVSession {
     /// Uploads the data with the given request.
     /// - Parameters: request: The request to upload the data with.
     /// - Parameters: data: The data to upload.
-    /// - Returns: The response.
+    /// - Returns: The Http response.
     @discardableResult public func upload(request: URLRequest, data: Data) async throws -> HTTPURLResponse {
         let (data, urlResponse) = try await self.urlSession.upload(for: request, from: data)
         
@@ -33,7 +33,7 @@ extension WebDAVSession {
     /// Uploads the data with the given request.
     /// - Parameters: request: The request to upload the data with.
     /// - Parameters: fileURL: The file URL from which to upload the data.
-    /// - Returns: The response.
+    /// - Returns: The Http response.
     @discardableResult public func upload(request: URLRequest, fromFile fileURL: URL) async throws -> HTTPURLResponse {
         let (data, urlResponse) = try await self.urlSession.upload(for: request, fromFile: fileURL)
         
@@ -50,6 +50,7 @@ extension WebDAVSession {
     /// - Parameters: query: The query to use for the request.
     /// - Parameters: modifiedTime: The modified time of the file. Only used for Owncloud/Nextcloud.
     /// - Parameters: account: The account used to authorize the request.
+    /// - Returns: The Http response.
     @discardableResult public func upload(to path: any WebDAVPathProtocol, 
                                           data: Data, contentType: MimeType,
                                           headers: [String: String]? = nil, query: [String: String]? = nil,
@@ -78,6 +79,7 @@ extension WebDAVSession {
     /// - Parameters: query: The query to use for the request.
     /// - Parameters: modifiedTime: The modified time of the file. Only used for Owncloud/Nextcloud.
     /// - Parameters: account: The account used to authorize the request.
+    /// - Returns: The Http response.
     @discardableResult public func upload(to path: any WebDAVPathProtocol,
                                           fromFile fileURL: URL, contentType: MimeType,
                                           headers: [String: String]? = nil, query: [String: String]? = nil,
@@ -95,5 +97,34 @@ extension WebDAVSession {
         }
         
         return try await upload(request: request, fromFile: fileURL)
+    }
+    
+    /// Creates an upload task to upload data from a file to the given path. Suitable for background uploads.
+    /// - Parameters: path: The path to upload the data to.
+    /// - Parameters: fileURL: The file URL from which to upload the data.
+    /// - Parameters: contentType: The content type of the data.
+    /// - Parameters: headers: Any additional headers to use for the request.
+    /// - Parameters: query: The query to use for the request.
+    /// - Parameters: modifiedTime: The modified time of the file. Only used for Owncloud/Nextcloud.
+    /// - Parameters: account: The account used to authorize the request.
+    /// - Returns: The upload task.
+    /// - Note: The task is not started automatically. You need to call `resume()` on it.
+    public func createUploadTask(to path: any WebDAVPathProtocol,
+                                 fromFile fileURL: URL, contentType: MimeType,
+                                 headers: [String: String]? = nil, query: [String: String]? = nil,
+                                 modifiedTime: Date?,
+                                 account: any WebDAVAccount) throws -> URLSessionUploadTask {
+        
+        var request = try self.authorizedRequest(method: .put, filePath: path,
+                                                 query: query, headers: headers,
+                                                 contentType: contentType,
+                                                 ocsApiRequest: account.serverType.isOwncloud && modifiedTime != nil,
+                                                 account: account)
+
+        if let modifiedTime = modifiedTime, account.serverType.isOwncloud {
+            request.addValue("\(Int(modifiedTime.timeIntervalSince1970))", forHTTPHeaderField: "X-OC-Mtime")
+        }
+        
+        return self.urlSession.uploadTask(with: request, fromFile: fileURL)
     }
 }
